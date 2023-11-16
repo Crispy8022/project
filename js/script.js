8a0bc1ed4f5cfdc9b38fcadc9cbfffb7a1f66c95
Ivan <example@gmail.com>
2023-11-16 04:55:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// }
+
+// console.log(persone["name"]);
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+// console.log(arr[2])
+
+// alert("Hello");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Do u have 18 year?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+// console.log(4 + +" - object");
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr++;
+
+console.log(incr, decr);
+
+console.log(5%3);
+console.log("2" == 2);
+
+let isChecked = false;
+let isClose = true;
+
+console.log(isChecked || !isClose)
